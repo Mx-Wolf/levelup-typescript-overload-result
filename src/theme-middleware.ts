4a2b5f3a0ee3914781714f81a39dd07b29c90643
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-01-30 18:50:47 +0300

    implements the function to apply theme css class to an arbitry html element

--- src/theme-middleware.ts
+++ src/theme-middleware.ts
@@ -1,6 +1,6 @@
 let themeName: string | undefined;
 export const setTheme = (name: string) => themeName = name;
-export const withThemeClass = (element:HTMLElement)=>{
+export const withTheme = (element:HTMLElement)=>{
   if(typeof themeName === 'string'){
     element.classList.add(themeName);
   }
