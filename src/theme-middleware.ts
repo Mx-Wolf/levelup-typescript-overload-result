c47cd838d60f4e5512d3b9c9ee5fc15c4343d133
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-02-07 10:27:03 +0300

    lint

--- src/theme-middleware.ts
+++ src/theme-middleware.ts
@@ -1,10 +1,12 @@
-import { ThemeMiddleware } from "./i-face-theme-middleware";
+import { ThemeMiddleware } from './i-face-theme-middleware';
 
 let themeName: string | undefined;
-export const setTheme = (name: string) => themeName = name;
+
+export const setTheme = (name: string) => {themeName = name;};
+
 export const withTheme:ThemeMiddleware = (element)=>{
   if(typeof themeName === 'string'){
     element.classList.add(themeName);
   }
-  return element as any;
-}
+  return element as never;
+};
